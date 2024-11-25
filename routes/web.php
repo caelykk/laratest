6dd246cedf5e3e46508a946861e8de26af32bce7
caelykk <caelykkaty.m@gmail.com>
2024-11-25 17:14:10 +0300

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -1,50 +1,18 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-	return view('home');
-});
-Route::get('/delivery-payment', function () {
-	return view('delivery-payment/delivery');
-});
-
-Route::get('/address', function () {
-	return view('address/address');
-});
-Route::get('/delayed', function () {
-	return view('delayed/delayed');
-});
-
-Route::get('/personal', function () {
-	return view('personal/cabinet');
-});
-
-Route::get('/cart', function () {
-	return view('cart/cart');
-});
-
-Route::get('/catalog', function () {
-	return view('catalog/catalog');
-});
-
-Route::get('/authors', function () {
-	return view('authors/authors');
-});
-
-Route::get('/new', function () {
-	return view('new/new');
-});
-
-Route::get('/sale', function () {
-	return view('sale/sale');
-});
-
-Route::get('/partners', function () {
-	return view('partners/partners');
-});
-
-Route::get('/about', function () {
-	return view('about/about');
-});
+Route::get('/', [MainController::class, 'index'])->name('home');
+Route::get('/delivery-payment', [MainController::class, 'delivery_payment'])->name('delivery-payment');
+Route::get('/address', [MainController::class, 'address'])->name('address');
+Route::get('/delayed', [MainController::class, 'delayed'])->name('delayed');
+Route::get('/personal', [MainController::class, 'personal'])->name('personal');
+Route::get('/cart', [MainController::class, 'cart'])->name('cart');
+Route::get('/catalog', [MainController::class, 'catalog'])->name('catalog');
+Route::get('/authors', [MainController::class, 'authors'])->name('authors');
+Route::get('/new', [MainController::class, 'new'])->name('new');
+Route::get('/sale', [MainController::class, 'sale'])->name('sale');
+Route::get('/partners', [MainController::class, 'partners'])->name('partners');
+Route::get('/about', [MainController::class, 'about'])->name('about');
